0de732fc7a4ce6b48a132c9be855bde1bb34b3e0
Victor Rocha <victordsrocha@gmail.com>
2020-09-03 19:07:28 -0300

    Assets importados
    Durante o estudo dos tutoriais de tilemaps criei um grid inicial e um sistema de visão

---
+++ IDEAL/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private VisionMultipleRays fieldOfView;
+    public float speed;
+    private Rigidbody2D myRigidbody;
+    private Vector3 change;
+    private Animator animator;
+    private int direction = 4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        change = Vector3.zero;
+        // se preferir com aceleração basta trocar o GetAxisRaw por GetAxis
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+        direction = SetDirection();
+        fieldOfView.SetAimDirection(SetVectorDirection());
+        fieldOfView.SetOrigin(transform.position);
+        UpdateAnimationAndMove();
+    }
+
+    void UpdateAnimationAndMove()
+    {
+        if (change != Vector3.zero)
+        {
+            MoveCharacter();
+            animator.SetFloat("moveX", change.x);
+            animator.SetFloat("moveY", change.y);
+            animator.SetBool("moving", true);
+        }
+        else
+        {
+            animator.SetBool("moving", false);
+        }
+    }
+
+    void MoveCharacter()
+    {
+        myRigidbody.MovePosition(
+            transform.position + change * speed * Time.deltaTime
+        );
+    }
+
+    float SetVectorDirection()
+    {
+        float a = 0f;
+        if (this.direction == 0)
+        {
+            a = 90f;
+        }
+        else if (this.direction == 1)
+        {
+            a = 45f;
+        }
+        else if (this.direction == 2)
+        {
+            a = 0f;
+        }
+        else if (this.direction == 3)
+        {
+            a = -45f;
+        }
+        else if (this.direction == 4)
+        {
+            a = -90f;
+        }
+        else if (this.direction == 5)
+        {
+            a = -135f;
+        }
+        else if (this.direction == 6)
+        {
+            a = 180f;
+        }
+        else if (this.direction == 7)
+        {
+            a = 135f;
+        }
+
+        return a;
+    }
+
+    int SetDirection()
+    {
+        int dir = -1;
+        if (change.x > 0)
+        {
+            if (change.y > 0)
+            {
+                dir = 1;
+            }
+            else if (change.y == 0)
+            {
+                dir = 2;
+            }
+            else
+            {
+                dir = 3;
+            }
+        }
+        else if (change.x == 0)
+        {
+            if (change.y > 0)
+            {
+                dir = 0;
+            }
+            else if (change.y == 0)
+            {
+                // retorna get direction
+                dir = this.direction;
+            }
+            else
+            {
+                dir = 4;
+            }
+        }
+        else if (change.x < 0)
+        {
+            if (change.y > 0)
+            {
+                dir = 7;
+            }
+            else if (change.y == 0)
+            {
+                dir = 6;
+            }
+            else
+            {
+                dir = 5;
+            }
+        }
+
+        return dir;
+    }
+}
---
+++ IDEAL/Assets/Scripts/VisionMultipleRays.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisionMultipleRays : MonoBehaviour
+{
+    [SerializeField] private LayerMask layerMask;
+    private float fov;
+    private Vector3 origin;
+    private float startingAngle;
+    [SerializeField] private List<Color> visionState;
+
+    private void Start()
+    {
+        fov = 90f;
+        origin = Vector3.zero;
+    }
+
+    private void LateUpdate()
+    {
+        int rayCount = 11;
+        float angle = startingAngle;
+        float angleIncrease = fov / rayCount;
+        float viewDistance = 100f;
+
+        visionState = new List<Color>();
+        for (int i = 0; i <= rayCount; i++)
+        {
+            visionState.Add(Color.black);
+        }
+
+        for (int i = 0; i <= rayCount; i++)
+        {
+            Vector3 vertex;
+            RaycastHit2D raycastHit2D = Physics2D.Raycast(origin, GetVectorFromAngle(angle), viewDistance, layerMask);
+            if (raycastHit2D.collider == null)
+            {
+                // No hit
+                vertex = GetVectorFromAngle(angle) * viewDistance;
+            }
+            else
+            {
+                // Hit object
+                vertex = raycastHit2D.point;
+                vertex = vertex - origin;
+
+                if (raycastHit2D.collider.name == "Trees_Tilemap")
+                {
+                    visionState[i] = Color.green;
+                }
+                else if (raycastHit2D.collider.name == "Rocks_Tilemap")
+                {
+                    visionState[i] = Color.gray;
+                }
+                else if (raycastHit2D.collider.name == "House_Tilemap")
+                {
+                    visionState[i] = new Color(0.6f, 0.33f, 0.17f);
+                }
+                else if (raycastHit2D.collider.name == "Fountain_Tilemap")
+                {
+                    visionState[i] = Color.blue;
+                }
+                else if (raycastHit2D.collider.name == "Benchs_Tilemap")
+                {
+                    visionState[i] = new Color(0.71f, 0.27f, 0.6f);
+                }
+                else if (raycastHit2D.collider.name == "Block_Tilemap")
+                {
+                    visionState[i] = Color.black;
+                }
+            }
+
+            Debug.DrawRay(origin, vertex, Color.magenta);
+
+            angle -= angleIncrease;
+        }
+    }
+
+
+    public static Vector3 GetVectorFromAngle(float angle)
+    {
+        // angle = 0 -> 360
+        float angleRad = angle * (Mathf.PI / 180f);
+        return new Vector3(Mathf.Cos(angleRad), Mathf.Sin(angleRad));
+    }
+
+    public static float GetAngleFromVectorFloat(Vector3 dir)
+    {
+        // dir -> direction
+        dir = dir.normalized;
+        float n = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        if (n < 0) n += 360;
+        return n;
+    }
+
+    public void SetOrigin(Vector3 origin)
+    {
+        this.origin = origin;
+    }
+
+    public void SetAimDirection(float aimDirection)
+    {
+        startingAngle = aimDirection + fov / 2f;
+    }
+}
